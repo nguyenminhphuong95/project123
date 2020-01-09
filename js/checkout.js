8442b57f80c3ec72f288076da1270b98e9a990cc
hongky-1994 <hongkyhvnh@gmail.com>
2020-01-09 22:04:52 +0700

    index

--- js/checkout.js
+++ js/checkout.js
@@ -37,25 +37,23 @@ function displayCartItems(){
                 <p>- Giảm giá: ${discount} %</p>
                 <button type="button" class="btn btn-outline-danger btn-sm" onclick="remove_cart_item(${i})">Xóa</button>
             </div>
-            <div class="col-sm-1 cart-row" >
-                <input style="width: 50px" id="cart-item-quantity${i}" type="number" value ="${e.quantity}" min="1" onchange="changeQuantity(${i})">
+            <div class="col-sm-2 cart-row" >
+                <input style="width: 40px" id="cart-item-quantity${i}" type="number" value ="${e.quantity}" min="1" onchange="changeQuantity(${i})">
             </div>
     
-            <div class="col-sm-4 cart-row" style="text-align: right;" id="sum${i}">
-                ${format_curency(sum)}đ
-            </div>
+            <div class="col-sm-3 cart-row" style="text-align: right;" id="sum${i}">${format_curency(sum)}đ</div>
         </div>
         `;
     }
     cartRows +=`
         <div class="row">
             <div class="col-sm-9 cart-row"> VAT </div>
-            <div class="col-sm-3 cart-row" style="text-align: right;" id="VAT${i}">${format_curency(totalSum*0.1)} đ</div>
+            <div class="col-sm-3 cart-row" style="text-align: right;" id="VAT">${format_curency(totalSum*0.1)}đ</div>
             
         </div>        
         <div class="row">
             <div class="col-sm-9 cart-row"  style="border-top: none;"> Tổng tiền (VAT) </div>
-            <div class="col-sm-3 cart-row" style="text-align: right; border-top: none;" id="totalSum${i}">${format_curency(totalSum*1.1)}đ</div>
+            <div class="col-sm-3 cart-row" style="text-align: right; border-top: none;" id="totalSum">${format_curency(totalSum*1.1)}đ</div>
             
         </div>
     `
@@ -75,19 +73,30 @@ function searchCartItem(a,element) {
 
 // --------------------------------------------------------------------------- Đổi khi số lượng thay đổi
 function changeQuantity(a){
-    let quantity = document.getElementById(`cart-item-quantity${a}`).value;
-    cartItem[a].quantity = Number(quantity);
-    let price = searchCartItem(cartItem[a].no,"price");
-    let sum = quantity * price;
-    console.log(`${format_curency(sum)}đ`);
+    let oldSum = document.getElementById(`sum${a}`).innerHTML;
+    let oldTotalVAT = document.getElementById('totalSum').innerHTML.toString();
+    console.log(oldTotalVAT);
     
-    document.getElementById(`sum${a}`).innerHTML = `${format_curency(sum)}đ`;
+    oldSum = parseInt(convertTextToNumber(oldSum));
+    oldTotalVAT = parseInt(convertTextToNumber(oldTotalVAT));
  
+    cartItem[a].quantity = Number(document.getElementById(`cart-item-quantity${a}`).value);
+    let price = searchCartItem(cartItem[a].no,"price");
+    let newSum = cartItem[a].quantity * price;
+
+    // change sum từng dòng
+    document.getElementById(`sum${a}`).innerHTML = `${format_curency(newSum)}đ`;
+    // change total giỏ hàng
 
 
+    console.log("oldSum = "+oldSum);
+    console.log("newSum = "+ newSum);
+    console.log("oldTotalVAT = "+oldTotalVAT);
+    let newTotalVAT = ((oldTotalVAT / 1.1) - oldSum + newSum)*1.1;
 
+    document.getElementById(`totalSum`).innerHTML = format_curency(newTotalVAT)+"đ";
+    console.log("newTotalVAT = "+newTotalVAT);
     
-    document.getElementById(`totalSum${i}`)
     savecart();    
 
     
@@ -117,4 +126,9 @@ function add (){
     cartItem.push(item);
     savecart()
 }
-// add()
+function convertTextToNumber (str){
+    str=str.toString();
+    str = str.replace(/,/g,"");  
+    str = parseInt(str.subString());
+    return str;  
+}
